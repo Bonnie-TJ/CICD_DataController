0adf398fcd28f3e367ed44eb97713314a797eb9a
TangJing <e1335039@u.nus.edu>
2024-01-22 14:46:20 +0800

    Analyse workflow with Sonar - disabled Automatic Analysis in Sonar

---
+++ src/test/java/sg/edu/nus/iss/d13revision/PersonControllerTest.java
@@ -0,0 +1,30 @@
+package sg.edu.nus.iss.d13revision;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import sg.edu.nus.iss.d13revision.controllers.PersonController;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class PersonControllerTest {
+
+    PersonController personController;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void testShowAddPersonPage() {
+
+        assertEquals("HEALTH CHECK OK!", personController.showAddPersonPage(null));
+    }
+
+    @Test
+    void testGetAllPersons() {
+
+    }
+}
